f6cc15bbe0d309f8c00e83e97b9729bfba95b3f0
Rustem <r-box97@mail.ru>
2022-02-24 15:12:42 +0300

    i creat kurs project

---
+++ src/main/java/com/example/sitaris_kp/SitarisKpApplication.java
@@ -0,0 +1,13 @@
+package com.example.sitaris_kp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitarisKpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitarisKpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sitaris_kp/SitarisKpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sitaris_kp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitarisKpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
