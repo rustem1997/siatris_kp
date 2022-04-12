b1eef0346926b349411527d3b193a33d338b96d9
Rustem <r-box97@mail.ru>
2022-04-12 16:28:21 +0300

    создал CompanyRepository для запрос из MySql

---
+++ src/main/java/com/example/sitaris_kp/repository/CompanyRepository.java
@@ -0,0 +1,29 @@
+package com.example.sitaris_kp.repository;
+
+
+import com.example.sitaris_kp.model.Company;
+import lombok.Setter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+@Repository
+public interface CompanyRepository extends CrudRepository<Company, Long> {
+
+    Page<Company> findAll(Pageable pageable);
+
+    @Query("FROM Company c WHERE c.name LIKE %:searchText% OR c.areaOfWork LIKE %:searchText%")
+    Page<Company> findAllCompany(Pageable pageable, @Param("searchText") String searchText);
+
+    Set<Company> findAll();
+
+    Set<Company> findCompanyByUserSetId(Long Id);
+
+    Company findByVacancyListId(Long id);
+
+}
